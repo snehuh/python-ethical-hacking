a43e094a764be5cf5326f08a3b666937f0491149
Sneha B <snehababu0202@gmail.com>
2024-10-29 18:17:37 +0800

    Update simulator.py and add .gitignore
    bugs and commenting

--- simulator.py
+++ simulator.py
@@ -1,13 +1,15 @@
 import random
 
 def generate_random_ip():
-    return f"192.168.1.{random.randint(0, 20)}"
+    return f"192.168.1.{random.randint(1, 20)}"
 
 def check_firewall_rules(ip, rules):
+    # arguments: ip_address and firewall_rules dictionary
     for rule_ip, action in rules.items():
         if ip == rule_ip:
             return action  # will returm "block"
         return "allow"  # otherwise, allow
+
 def main():
     firewall_rules = {
         "192.168.1.1": "block",
@@ -15,7 +17,11 @@ def main():
         "192.168.1.9": "block",
         "192.168.1.13": "block",
         "192.168.1.16": "block",
-        "192.168.1.19": "block"
+        "192.168.1.11": "block",
+        "192.168.1.14": "block",
+        "192.168.1.15": "block",
+        "192.168.1.19": "block",
+        "192.168.1.17": "block"
     }
     
     # to simulate network traffic
@@ -23,7 +29,7 @@ def main():
         ip_address = generate_random_ip()  
         action = check_firewall_rules(ip_address, firewall_rules)
         random_number = random.randint(0, 9999)
-        print("IP: {ip_address}, Action: {action}, Random: {random_number}")
+        print(f"IP: {ip_address}, Action: {action}, Random: {random_number}")
 
 if __name__ == "__main__":
     main()
